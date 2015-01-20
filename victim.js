a55c774d78d65bcf78b3b3ed05d449fecf3c2a8d
Michael F <mfriedmanzz01@gmail.com>
2015-01-19 17:29:00 -0700

    more

---
+++ victim.js
@@ -0,0 +1,62 @@
+
+var numberofvictims = prompt("How many victims?");
+var numberofvolunteers = prompt("How many Volunteers?");
+
+// var Victims = {};
+var victimsArray = [];
+// var Volunteers = {};
+var volunteersArray =[];
+var 
+
+  function Victims2(fullname, phone, street) {
+	this.fullname = fullname;
+	this.phone = phone;
+	this.street = street;
+}
+
+function Volunteers2(fullname, phone, street){
+	this.fullname = fullname;
+	this.phone = phone;
+	this.street = street;
+}
+
+
+for (var i=0; i < numberofvictims.length; i++) {
+
+var fullname1 = prompt("What is the name of the first victim, Victim " + i);
+
+var phone1 = prompt("What is the phone number of victim " + i);
+
+var street1 = prompt("what is the street address of victim " + i);
+victimsArray[i] = new Victims2(fullname1, phone1, street1);
+}
+
+for (var x=0; x < numberofvolunteers.length; x++) {
+	var fullname2 = prompt("What is the name of  Volunteer " + x);
+
+	var phone2 = prompt("What is the Phone Number of Volunteer " + x);
+
+	var street2 = prompt("What is the Street addres of Vulunteer " + x);
+	
+	volunteersArray[x] = new Volunteers2(fullname2, phone2, street2);
+}
+
+
+for (var x = 0; x < Victims2.length; x++) {
+    console.log(Victims2[x].fullname);
+    console.log(Victims2[x].phone);
+    console.log(Victims2[x].street);
+}
+
+// console.log ("Here are all the victims listed ");
+//  var victimsall = Victims2.map(function (list) { return list;} );
+     
+
+//  console.log(victimsall);
+
+// console.log ("Here are all the volunteers Listed");
+
+// var volunteersall = Volunteers2.map(function (list){ return list;} );
+       
+ 
+//  console.log(victimsall);
